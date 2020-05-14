fec1127c3550549a6b9c7025800fb96c1c1ac185
Jack Quinoa <jquinoa208@gmail.com>
2020-05-14 20:22:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(name)
-  names =["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
   names.each do |name|
     return "Hello, my name is #{name}."
   end
