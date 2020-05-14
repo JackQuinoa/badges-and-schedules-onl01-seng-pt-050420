88487d19ded6c0847bcf64a47b3a04d5452a177f
Jack Quinoa <jquinoa208@gmail.com>
2020-05-14 20:24:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,19 +4,15 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(name)
 
-  names.each do |name|
+def batch_badge_creator(speakers)
+  speakers.each do |name|
     return "Hello, my name is #{name}."
   end
 end
 
 
-
-
-
-def assign_rooms(name,room)
-  names =["Edsger", "Ada", "Charles", "Linus", "Matz"]
-  rooms =[1,2,3,4,5,6,7]
+def assign_rooms(speakers,room)
+  room=[1,2,3,4,5,6,7]
   return "Hello, #{name}! You'll be asssigned to room #{room}!"
 end
