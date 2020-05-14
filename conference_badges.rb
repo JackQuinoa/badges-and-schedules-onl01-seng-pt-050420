6d0f744a6237698503988cec82ccc06a2d13f053
Jack Quinoa <jquinoa208@gmail.com>
2020-05-14 20:25:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,13 +1,13 @@
 # Write your code here.
   speakers =["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def badge_maker(name)
-  return "Hello, my name is #{name}."
+  "Hello, my name is #{name}."
 end
 
 
 def batch_badge_creator(speakers)
   speakers.each do |name|
-    return "Hello, my name is #{name}."
+    "Hello, my name is #{name}."
   end
 end
 
